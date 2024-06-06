5f93f6fa5f4c88e692ff1f763813dca3178dc63f
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-06-06 18:44:09 +0200

    Update ConfigManager.cs

--- WeatherRegistry/ConfigManager.cs
+++ WeatherRegistry/ConfigManager.cs
@@ -6,20 +6,33 @@ namespace WeatherRegistry
   public class ConfigManager
   {
     public static ConfigManager Instance { get; private set; }
-    internal readonly ConfigFile configFile;
+    internal static ConfigFile configFile;
 
     public static void Init(ConfigFile config)
     {
       Instance = new ConfigManager(config);
     }
 
+    // general settings
+
+    // map screen settings
     public static ConfigEntry<bool> ColoredWeathers { get; private set; }
+
+    // sun animator settings
+    private static ConfigEntry<string> SunAnimatorBlacklist { get; set; }
+    public static SelectableLevel[] SunAnimatorBlacklistLevels => ConfigHelper.ConvertStringToLevels(SunAnimatorBlacklist.Value);
 
     private ConfigManager(ConfigFile config)
     {
       configFile = config;
 
-      ColoredWeathers = configFile.Bind("General", "Colored Weathers", true, "Enable colored weathers in map screen");
+      ColoredWeathers = configFile.Bind("|General", "Colored Weathers", true, "Enable colored weathers in map screen");
+      SunAnimatorBlacklist = configFile.Bind(
+        "|SunAnimator",
+        "Blacklist",
+        "Asteroid-13;",
+        "Semicolon-separated list of level names to blacklist from being patched by sun animator"
+      );
     }
   }
 }
