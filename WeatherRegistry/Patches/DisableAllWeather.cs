30896f6b53aa88d005802a2db46b06adb23258eb
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-06-15 12:00:58 +0200

    Create DisableAllWeather.cs

---
+++ WeatherRegistry/Patches/DisableAllWeather.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace WeatherRegistry.Patches
+{
+  [HarmonyPatch(typeof(TimeOfDay))]
+  public static partial class TimeOfDayPatch
+  {
+    internal static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("WeatherTweaks TimeOfDay");
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(TimeOfDay), "DisableAllWeather")]
+    private static void DisableAllWeatherPatch(TimeOfDay __instance, bool deactivateObjects)
+    {
+      logger.LogDebug("Disabling all weather");
+
+      if (!deactivateObjects)
+      {
+        return;
+      }
+
+      logger.LogDebug("DecativateObjects is true");
+
+      foreach (ImprovedWeatherEffect effect in WeatherManager.Weathers.Select(weather => weather.Effect))
+      {
+        effect.DisableEffect(deactivateObjects);
+      }
+
+      EventManager.DisableAllWeathers.Invoke();
+
+      WeatherRegistry.Patches.SunAnimator.Clear();
+    }
+  }
+}
