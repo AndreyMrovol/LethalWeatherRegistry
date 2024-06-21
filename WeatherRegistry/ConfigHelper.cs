a19925aa3b1aceb4ab8b61f80b8ffafaed0bb3f7
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-06-21 23:33:42 +0200

    ConfigHandlers for level and weather weights

--- WeatherRegistry/ConfigHelper.cs
+++ WeatherRegistry/ConfigHelper.cs
@@ -1,16 +1,123 @@
 using System.Collections.Generic;
 using System.Linq;
+using BepInEx.Configuration;
 
 namespace WeatherRegistry
 {
   public class Rarity
   {
-    public string Name { get; set; }
     public int Weight { get; set; }
+  }
+
+  public class NameRarity : Rarity
+  {
+    public string Name { get; set; }
+  }
+
+  public class LevelRarity : Rarity
+  {
+    public SelectableLevel Level { get; set; }
+  }
+
+  public class WeatherRarity : Rarity
+  {
+    public Weather Weather { get; set; }
+  }
+
+  public class ConfigHandler<T, CT>
+  {
+    // i need to make some stupid shit
+    // basically a property that wraps around Bepinex ConfigEntry
+    // so a getter would read that and resolve to my types
+    // and a setter that does <something>
+
+    // and this will resolve to string
+    // so bepinex won't shit the bed hehe
+
+    internal ConfigEntry<CT> _configEntry { get; set; }
+
+    internal CT DefaultValue { get; set; }
+
+    public virtual T Value { get; } = default(T);
+
+    // forgive me for doing this
+  }
+
+  public class LevelListConfigHandler : ConfigHandler<SelectableLevel[], string>
+  {
+    public override SelectableLevel[] Value
+    {
+      get
+      {
+
+        return ConfigHelper.ConvertStringToLevels(_configEntry.Value);
+      }
+    }
+  }
+
+  public class LevelWeightsConfigHandler : ConfigHandler<LevelRarity[], string>
+  {
+    public override LevelRarity[] Value
+    {
+      get
+      {
+
+        return ConfigHelper.ConvertStringToLevelRarities(_configEntry.Value);
+      }
+    }
+  }
+
+  public class WeatherWeightsConfigHandler : ConfigHandler<WeatherRarity[], string>
+  {
+    public override WeatherRarity[] Value
+    {
+      get
+      {
+
+        return ConfigHelper.ConvertStringToWeatherWeights(_configEntry.Value);
+      }
+    }
+  }
+
+  public class IntegerConfigHandler : ConfigHandler<int, int>
+  {
+    public override int Value
+    {
+      get { return _configEntry.Value; }
+    }
   }
 
   internal class ConfigHelper
   {
+    internal static Dictionary<string, SelectableLevel> _dictionary = null;
+    public static Dictionary<string, SelectableLevel> StringToLevel
+    {
+      get
+      {
+        if (_dictionary != null)
+        {
+          return _dictionary;
+        }
+
+        Dictionary<string, SelectableLevel> Levels = [];
+
+        StartOfRound
+          .Instance.levels.ToList()
+          .ForEach(level =>
+          {
+            Levels.TryAdd(GetNumberlessName(level).ToLower(), level);
+            Levels.TryAdd(level.PlanetName.ToLower(), level);
+            Levels.TryAdd(level.name.ToLower(), level);
+          });
+
+        _dictionary = Levels;
+
+        // return the result of this setter
+        return Levels;
+      }
+    }
+
+    // straight-up copied from LLL (it's so fucking useful)
     public static string GetNumberlessName(SelectableLevel level)
     {
       return new string(level.PlanetName.SkipWhile(c => !char.IsLetter(c)).ToArray());
@@ -27,18 +134,9 @@ namespace WeatherRegistry
     public static SelectableLevel[] ConvertStringToLevels(string str)
     {
       string[] levelNames = ConvertStringToArray(str);
-      Dictionary<string, SelectableLevel> Levels = [];
+      Dictionary<string, SelectableLevel> Levels = StringToLevel;
       // StartOfRound.Instance.levels.ToDictionary(level => GetNumberlessName(level), level => level);
       List<SelectableLevel> output = [];
-
-      StartOfRound
-        .Instance.levels.ToList()
-        .ForEach(level =>
-        {
-          Levels.TryAdd(GetNumberlessName(level), level);
-          Levels.TryAdd(level.PlanetName, level);
-          Levels.TryAdd(level.name, level);
-        });
 
       if (levelNames.Count() == 0)
       {
@@ -62,10 +160,10 @@ namespace WeatherRegistry
       return output.ToArray();
     }
 
-    public Rarity[] ConvertStringToRarities(string str)
+    public static NameRarity[] ConvertStringToRarities(string str)
     {
       string[] rarities = ConvertStringToArray(str);
-      List<Rarity> output = [];
+      List<NameRarity> output = [];
 
       foreach (string rarity in rarities)
       {
@@ -83,7 +181,83 @@ namespace WeatherRegistry
           continue;
         }
 
-        output.Add(new Rarity { Name = rarityData[0], Weight = weight });
+        output.Add(new NameRarity { Name = rarityData[0], Weight = weight });
+      }
+
+      return output.ToArray();
+    }
+
+    public static LevelRarity[] ConvertStringToLevelRarities(string str)
+    {
+      // i want to use the following format:
+      // LevelName@Weight;LevelName@Weight;LevelName@Weight
+
+      string[] rarities = ConvertStringToArray(str);
+      List<LevelRarity> output = [];
+
+      foreach (string rarity in rarities)
+      {
+        string[] rarityData = rarity.Split('@');
+
+        if (rarityData.Length != 2)
+        {
+          Plugin.logger.LogError($"Invalid rarity data: {rarity}");
+          continue;
+        }
+
+        if (!int.TryParse(rarityData[1], out int weight))
+        {
+          Plugin.logger.LogError($"Invalid rarity weight: {rarityData[1]}");
+          continue;
+        }
+
+        SelectableLevel level = StartOfRound.Instance.levels.FirstOrDefault(l => l.PlanetName == rarityData[0]);
+
+        if (level == null)
+        {
+          Plugin.logger.LogError($"Invalid level name: {rarityData[0]}");
+          continue;
+        }
+
+        output.Add(new LevelRarity { Level = level, Weight = weight });
+      }
+
+      return output.ToArray();
+    }
+
+    public static WeatherRarity[] ConvertStringToWeatherWeights(string str)
+    {
+      // i want to use the following format:
+      // Weather@Weight;Weather@Weight;Weather@Weight
+
+      string[] rarities = ConvertStringToArray(str);
+      List<WeatherRarity> output = [];
+
+      foreach (string rarity in rarities)
+      {
+        string[] rarityData = rarity.Split('@');
+
+        if (rarityData.Length != 2)
+        {
+          Plugin.logger.LogError($"Invalid rarity data: {rarity}");
+          continue;
+        }
+
+        if (!int.TryParse(rarityData[1], out int weight))
+        {
+          Plugin.logger.LogError($"Invalid rarity weight: {rarityData[1]}");
+          continue;
+        }
+
+        Weather weather = WeatherManager.Weathers.FirstOrDefault(w => w.name == rarityData[0]);
+
+        if (weather == null)
+        {
+          Plugin.logger.LogError($"Invalid weather name: {rarityData[0]}");
+          continue;
+        }
+
+        output.Add(new WeatherRarity { Weather = weather, Weight = weight });
       }
 
       return output.ToArray();
---
+++ WeatherRegistry/Definitions/WeatherConfig.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using BepInEx.Configuration;
+using Newtonsoft.Json;
+
+namespace WeatherRegistry
+{
+  public class WeatherConfig
+  {
+    [JsonIgnore]
+    internal IntegerConfigHandler DefaultWeight;
+
+    [JsonIgnore]
+    internal IntegerConfigHandler ScrapAmountMultiplier;
+
+    [JsonIgnore]
+    internal IntegerConfigHandler ScrapValueMultiplier;
+
+    [JsonIgnore]
+    internal ConfigEntry<bool> _filteringOptionConfig { get; private set; }
+
+    [JsonIgnore]
+    internal LevelListConfigHandler LevelFilters;
+
+    [JsonIgnore]
+    internal LevelWeightsConfigHandler LevelWeights;
+
+    [JsonIgnore]
+    internal WeatherWeightsConfigHandler WeatherToWeatherWeights;
+
+    internal void Init(Weather weather)
+    {
+      string configCategory = $"Weather: {weather.name}{(weather.Origin != WeatherOrigin.Vanilla ? $" ({weather.Origin})" : "")}";
+
+      // this._defaultWeightConfig = ConfigManager.configFile.Bind(
+      //   configCategory,
+      //   $"Default weight",
+      //   weather.DefaultWeight,
+      //   new ConfigDescription("The default weight of this weather", new AcceptableValueRange<int>(0, 10000))
+      // );
+
+      DefaultWeight = new()
+      {
+        _configEntry = ConfigManager.configFile.Bind(
+          configCategory,
+          $"Default weight",
+          weather.DefaultWeight,
+          new ConfigDescription("The default weight of this weather", new AcceptableValueRange<int>(0, 10000))
+        ),
+        DefaultValue = weather.DefaultWeight,
+      };
+
+      this._filteringOptionConfig = ConfigManager.configFile.Bind(
+        configCategory,
+        $"Filtering option",
+        weather.LevelFilteringOption == FilteringOption.Include,
+        new ConfigDescription("Whether to make the filter a whitelist (false is blacklist, true is whitelist)", null)
+      );
+
+      LevelFilters = new()
+      {
+        _configEntry = ConfigManager.configFile.Bind(
+          configCategory,
+          $"Level filter",
+          $"{String.Join(";", weather.DefaultLevelFilters)};",
+          new ConfigDescription("Semicolon-separated list of level names to filter", null)
+        ),
+        DefaultValue = $"{String.Join(";", weather.DefaultLevelFilters)};",
+      };
+
+      LevelWeights = new()
+      {
+        _configEntry = ConfigManager.configFile.Bind(
+          configCategory,
+          $"Level weights",
+          $"LevelName@Weight;",
+          new ConfigDescription("Semicolon-separated list of level names to filter", null)
+        ),
+        DefaultValue = "",
+      };
+
+      WeatherToWeatherWeights = new()
+      {
+        _configEntry = ConfigManager.configFile.Bind(
+          configCategory,
+          $"Weather weights",
+          $"WeatherName@Weight;",
+          new ConfigDescription("Semicolon-separated list of level names to filter", null)
+        ),
+        DefaultValue = "",
+      };
+    }
+  }
+}
