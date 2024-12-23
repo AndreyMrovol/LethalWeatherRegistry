1ebdf3642a1d6a91a64eab112734269d65a6bd28
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-12-23 19:38:00 +0100

    Settings are setting things now

--- WeatherRegistry/Settings.cs
+++ WeatherRegistry/Settings.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Unity;
 using UnityEngine;
+using WeatherRegistry.Definitions;
 
 namespace WeatherRegistry
 {
@@ -8,6 +9,11 @@ namespace WeatherRegistry
   {
     public static Dictionary<string, Color> ScreenMapColors = [];
 
-    public static bool SelectWeathers = true;
+    public static bool SelectWeathers { get; set; } = true;
+
+    public static WeatherSelectionAlgorithm WeatherSelectionAlgorithm { get; set; } =
+      ConfigManager.UseWeatherWeights.Value ? WeatherCalculation.RegistryAlgorithm : WeatherCalculation.VanillaAlgorithm;
+
+    public static bool ScrapMultipliers { get; set; } = ConfigManager.UseScrapMultipliers.Value;
   }
 }
