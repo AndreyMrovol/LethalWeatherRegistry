6d03ef7d3c937d0c02bed8f0c6070401a64d1d7e
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-07-05 23:23:08 +0200

    WeatherOverrideManager

---
+++ WeatherRegistry/WeatherOverrideManager.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using WeatherRegistry.Definitions;
+
+namespace WeatherRegistry
+{
+  public static class WeatherOverrideManager
+  {
+    public static List<WeatherEffectOverride> WeatherEffectOverrides { get; internal set; } = [];
+    public static Dictionary<WeatherEffectOverride, string> PlanetOverrideNames { get; internal set; } = [];
+
+    public static WeatherEffectOverride GetCurrentWeatherOverride(SelectableLevel level, Weather weather)
+    {
+      weather.WeatherEffectOverrides.TryGetValue(level, out WeatherEffectOverride weatherEffectOverride);
+
+      return weatherEffectOverride;
+    }
+
+    public static string GetPlanetOverrideName(WeatherEffectOverride overrideEffect)
+    {
+      if (PlanetOverrideNames.TryGetValue(overrideEffect, out string planetName))
+      {
+        return planetName;
+      }
+
+      return null;
+    }
+  }
+}
