14415b0b248088a79f3d6db29fa224e8b5fd62cf
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-08-06 02:17:47 +0200

    Create AdditionalWeatherEffect.cs

---
+++ WeatherRegistry/Editor/AdditionalWeatherEffect.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace WeatherRegistry.Editor
+{
+  [CreateAssetMenu(fileName = "AdditionalWeatherEffect", menuName = "WeatherRegistry/Overrides/AdditionalWeatherEffect", order = 4)]
+  public class AdditionalWeatherEffect : ScriptableObject
+  {
+    [Header("Matching properties")]
+    [Tooltip("Weather names to match the override to")]
+    public string[] weatherName;
+
+    [Tooltip("Semicolon-separated list of level names to match the override to")]
+    public string[] levelName;
+
+    [Header("Effect properties")]
+    public ImprovedWeatherEffect AdditionalEffect;
+  }
+}
