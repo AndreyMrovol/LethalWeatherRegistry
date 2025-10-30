0671c16f819f424f999de25555a7e0a18708ac94
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-10-30 02:05:33 +0100

    Create OldWeatherType.cs

---
+++ WeatherRegistry/Deprecated/OldWeatherType.cs
@@ -0,0 +1,295 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using UnityEngine;
+using WeatherRegistry.Enums;
+using WeatherRegistry.Helpers;
+using WeatherRegistry.Modules;
+
+namespace WeatherRegistry
+{
+  [JsonObject(MemberSerialization.OptIn)]
+  [Obsolete]
+  public class Weather : ScriptableObject
+  {
+    #region Base properties
+
+    [JsonProperty]
+    public string Name;
+
+    [JsonIgnore]
+    public ImprovedWeatherEffect Effect;
+
+    [JsonProperty]
+    public LevelWeatherType VanillaWeatherType { get; internal set; } = LevelWeatherType.None;
+
+    [JsonIgnore]
+    internal WeatherOrigin Origin { get; set; } = WeatherOrigin.WeatherRegistry;
+
+    [JsonProperty]
+    public WeatherType Type { get; internal set; } = WeatherType.Modded;
+
+    [JsonIgnore]
+    public Dictionary<SelectableLevel, LevelWeatherVariables> WeatherVariables = [];
+
+    [JsonIgnore]
+    [Obsolete]
+    public AnimationClip AnimationClip;
+
+    [field: SerializeField]
+    public Color Color { get; set; } = Color.cyan;
+
+    [JsonIgnore]
+    public RegistryWeatherConfig Config = new();
+
+    [JsonIgnore]
+    internal Dictionary<SelectableLevel, Definitions.WeatherEffectOverride> WeatherEffectOverrides = [];
+
+    #endregion
+
+    #region backing fields
+
+    [Obsolete("Use Weather.Config.DefaultWeight instead")]
+    internal int _defaultWeight = 100;
+
+    [Obsolete("Use Weather.Config.ScrapAmountMultiplier instead")]
+    internal float _scrapAmountMultiplier = 1;
+
+    [Obsolete("Use Weather.Config.ScrapValueMultiplier instead")]
+    internal float _scrapValueMultiplier = 1;
+
+    #endregion
+
+    #region defaults
+
+    [property: SerializeField]
+    public int DefaultWeight
+    {
+      get { return Config.DefaultWeight.Value; }
+      [Obsolete("Use Weather.Config.DefaultWeight instead")]
+      set { _defaultWeight = value; }
+    }
+
+    [Obsolete("Use Weather.Config.LevelFilters instead")]
+    public string[] DefaultLevelFilters
+    {
+      get { return this.Config.LevelFilters.DefaultValue.Split(";"); }
+      set { this.Config.LevelFilters = new(value); }
+    }
+
+    [Obsolete("Use Weather.Config.LevelWeights instead")]
+    public string[] DefaultLevelWeights
+    {
+      get { return this.Config.LevelWeights.DefaultValue.Split(";"); }
+      set { this.Config.LevelWeights = new(value); }
+    }
+
+    [Obsolete("Use Weather.Config.WeatherToWeatherWeights instead")]
+    public string[] DefaultWeatherToWeatherWeights
+    {
+      get { return this.Config.WeatherToWeatherWeights.DefaultValue.Split(";"); }
+      set { this.Config.WeatherToWeatherWeights = new(value); }
+    }
+
+    public float ScrapAmountMultiplier
+    {
+      get { return Config.ScrapAmountMultiplier.Value; }
+      [Obsolete("Use Weather.Config.ScrapAmountMultiplier instead")]
+      set { Config.ScrapAmountMultiplier = new(value); }
+    }
+
+    public float ScrapValueMultiplier
+    {
+      get { return Config.ScrapValueMultiplier.Value; }
+      [Obsolete("Use Weather.Config.ScrapValueMultiplier instead")]
+      set { Config.ScrapValueMultiplier = new(value); }
+    }
+
+    #endregion
+
+    #region stuff from config
+
+    [property: SerializeField]
+    [JsonIgnore]
+    public FilteringOption LevelFilteringOption
+    {
+      get { return Config.FilteringOption.Value ? FilteringOption.Include : FilteringOption.Exclude; }
+      [Obsolete("Use Weather.Config.FilteringOption instead")]
+      set { Config.FilteringOption = new(value == FilteringOption.Include); }
+    }
+
+    [JsonIgnore]
+    public List<SelectableLevel> LevelFilters
+    {
+      get { return Config.LevelFilters.Value.ToList(); }
+    }
+
+    [JsonIgnore]
+    public Dictionary<LevelWeatherType, int> WeatherWeights
+    {
+      get { return Config.WeatherToWeatherWeights.Value.ToDictionary(rarity => rarity.Weather.VanillaWeatherType, rarity => rarity.Weight); }
+    }
+
+    [JsonIgnore]
+    public Dictionary<SelectableLevel, int> LevelWeights
+    {
+      get { return Config.LevelWeights.Value.ToDictionary(rarity => rarity.Level, rarity => rarity.Weight); }
+    }
+
+    #endregion
+
+    #region Constructor
+
+    public Weather(string name = "None", ImprovedWeatherEffect effect = default)
+    {
+      // // a small hack for Whimsical weather so it doesn't use <color> tags in their name
+      // Regex textTagsRegex = new(@"<.*?>");
+      // Name = textTagsRegex.Replace(name, "");
+      // this.name = textTagsRegex.Replace(name, "");
+
+      // Effect = effect;
+
+      // if (effect != null)
+      // {
+      //   Effect.name = name;
+      // }
+
+      // GameObject.DontDestroyOnLoad(this);
+
+      WeatherManager.RegisterWeather(
+        new Runtime.WeatherRuntimeDefinition(name, effect)
+        {
+          Color = ColorHelper.ToTMPColorGradient(this.Color),
+          Config = this.Config,
+          // Origin = this.Origin,
+          VanillaWeatherType = this.VanillaWeatherType
+        }
+      );
+    }
+
+    #endregion
+
+    internal virtual string ConfigCategory =>
+      $"{(this.Type == WeatherType.Vanilla || this.Type == WeatherType.Clear ? "Vanilla" : "Modded")} Weather: {this.name.Replace(" ", "")}{(this.Origin != WeatherOrigin.WeatherRegistry && this.Origin != WeatherOrigin.Vanilla ? $" ({this.Origin})" : "")}";
+
+    [Obsolete]
+    internal virtual void Init() { }
+
+    public override string ToString()
+    {
+      return Name;
+    }
+
+    public void RemoveFromMoon(string moonNames)
+    {
+      ConfigHelper.ConvertStringToLevels(moonNames).ToList().ForEach(level => LevelFilters.Remove(level));
+    }
+
+    public void RemoveFromMoon(SelectableLevel moon)
+    {
+      LevelFilters.Remove(moon);
+    }
+
+    public (int weight, WeatherWeightType type) GetWeightWithOrigin(SelectableLevel level)
+    {
+      Logger logger = WeatherCalculation.Logger;
+      var weatherWeight = this.DefaultWeight;
+      WeatherWeightType weatherWeightType = WeatherWeightType.Default;
+
+      var previousWeather = WeatherManager.GetWeather(level.currentWeather);
+
+      if (previousWeather == null)
+      {
+        logger.LogError($"Previous weather is null for {level.name}");
+      }
+
+      // we have 3 weights possible:
+      // 1. level weight
+      // 2. weather-weather weights
+      // 3. default weight
+      // we want to execute them in this exact order
+
+      if (this.LevelWeights.TryGetValue(level, out int levelWeight))
+      {
+        // (1) => level weight
+        // logger.LogDebug($"{this.Name} has level weight {levelWeight}");
+        weatherWeightType = WeatherWeightType.Level;
+        weatherWeight = levelWeight;
+      }
+      // try to get previous day weather (so - at this point - the current one)
+      // but not on first day because that's unreliable and random (from my testing)
+      else if (
+        previousWeather.WeatherWeights.TryGetValue(this.VanillaWeatherType, out int weatherWeightFromWeather)
+        && StartOfRound.Instance.gameStats.daysSpent != 0
+      )
+      {
+        // (2) => weather-weather weights
+
+        // logger.LogDebug($"{this.Name} has weather>weather weight {weatherWeightFromWeather}");
+        weatherWeightType = WeatherWeightType.WeatherToWeather;
+        weatherWeight = weatherWeightFromWeather;
+      }
+      else
+      {
+        weatherWeightType = WeatherWeightType.Default;
+        // logger.LogDebug($"{this.Name} has default weight {weatherWeight}");
+      }
+
+      return (weatherWeight, weatherWeightType);
+    }
+
+    public int GetWeight(SelectableLevel level)
+    {
+      return GetWeightWithOrigin(level).weight;
+    }
+
+    public string GetAlphanumericName()
+    {
+      return ConfigHelper.GetAlphanumericName(this.Name);
+    }
+
+    public (bool isWTW, int weight) GetWeatherToWeatherWeight(Weather previousWeather)
+    {
+      if (previousWeather.WeatherWeights.TryGetValue(this.VanillaWeatherType, out int weatherWeightFromWeather))
+      {
+        return (true, weatherWeightFromWeather);
+      }
+
+      return (false, DefaultWeight);
+    }
+
+    public Definitions.WeatherEffectOverride GetEffectOverride(SelectableLevel level)
+    {
+      if (WeatherEffectOverrides.ContainsKey(level))
+      {
+        return WeatherEffectOverrides[level];
+      }
+
+      return null;
+    }
+  }
+
+  public class LevelWeatherVariables
+  {
+    public SelectableLevel Level;
+
+    public int WeatherVariable1;
+    public int WeatherVariable2;
+  }
+
+  [Obsolete]
+  public class LevelWeather : LevelWeatherVariables
+  {
+    public Weather Weather;
+    public LevelWeatherVariables Variables;
+  }
+
+  public enum WeatherWeightType
+  {
+    Default,
+    WeatherToWeather,
+    Level,
+  }
+}
