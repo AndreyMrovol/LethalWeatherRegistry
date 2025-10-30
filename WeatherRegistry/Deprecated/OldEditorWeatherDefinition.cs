e74b199e202694fee032016c99485791c2ba6ad9
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-10-30 02:05:30 +0100

    Create OldEditorWeatherDefinition.cs

---
+++ WeatherRegistry/Deprecated/OldEditorWeatherDefinition.cs
@@ -0,0 +1,39 @@
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace WeatherRegistry.Editor
+{
+  // [JsonObject(MemberSerialization.OptIn)]
+  // [CreateAssetMenu(fileName = "Weather Definition", menuName = "WeatherRegistry/WeatherDefinition", order = 100)]
+  public class WeatherDefinition : ScriptableObject
+  {
+    [Header("Basic properties")]
+    [SerializeField]
+    [Tooltip("Your weather's name.")]
+    public string Name;
+
+    [SerializeField]
+    [Tooltip("Your weather's color, displayed on the landing screen.")]
+    public Color Color;
+
+    [Header("Effect")]
+    [Space(20)]
+    [SerializeField]
+    [Tooltip("Your weather's ImprovedWeatherEffect")]
+    public ImprovedWeatherEffect Effect;
+
+    [Header("Configuration")]
+    [SerializeField]
+    [Tooltip("Your weather's default config values.")]
+    public SerializableWeatherConfig Config;
+
+    public void Reset()
+    {
+      Name = string.Empty;
+      Effect = null;
+      Color = Color.cyan;
+
+      Config = new();
+    }
+  }
+}
