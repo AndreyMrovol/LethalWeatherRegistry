f6e3ccd1e84769a43b1266ef634cc53e84b76ee9
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-11-17 13:38:41 +0100

    Create WeatherType.cs

---
+++ WeatherRegistry/Enums/WeatherType.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace WeatherRegistry.Enums
+{
+  public enum WeatherType
+  {
+    Clear,
+    Vanilla,
+    Modded,
+  }
+
+  public enum WeatherOrigin
+  {
+    Vanilla,
+    WeatherRegistry,
+    LethalLib,
+    LethalLevelLoader,
+    DawnLib,
+    JacobLib,
+    WeatherTweaks,
+  }
+
+  public enum FilteringOption
+  {
+    Include,
+    Exclude,
+  }
+
+  public enum WeatherWeightType
+  {
+    Default,
+    WeatherToWeather,
+    Level,
+  }
+
+  public class LevelWeatherVariables
+  {
+    public SelectableLevel Level;
+
+    public int WeatherVariable1;
+    public int WeatherVariable2;
+  }
+
+  [Obsolete]
+  public class LevelWeather : LevelWeatherVariables
+  {
+    public ImprovedWeather Weather;
+    public LevelWeatherVariables Variables;
+  }
+}
