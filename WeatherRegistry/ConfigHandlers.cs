8dde8ce43e576aa48200de17b8ba3aac0139ef16
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-11-25 22:17:19 +0100

    enum shenanigans

--- WeatherRegistry/ConfigHandlers.cs
+++ WeatherRegistry/ConfigHandlers.cs
@@ -1,5 +1,6 @@
 using System;
 using BepInEx.Configuration;
+using WeatherRegistry.Enums;
 
 namespace WeatherRegistry
 {
--- WeatherRegistry/Patches/SunAnimator.cs
+++ WeatherRegistry/Patches/SunAnimator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using MrovLib;
 using UnityEngine;
+using WeatherRegistry.Enums;
 
 namespace WeatherRegistry.Patches
 {
