f1217088bd3d03d0d64b0094ebeb963e0624ce42
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-11-26 21:24:57 +0100

    small reorganization of WeatherManager

--- WeatherRegistry/WeatherManager.cs
+++ WeatherRegistry/WeatherManager.cs
@@ -15,9 +15,6 @@ namespace WeatherRegistry
 
     public static List<Weather> RegisteredWeathers { get; internal set; } = [];
 
-    [Obsolete("Use OverridesManager.WeatherEffectOverrides instead")]
-    public static List<WeatherEffectOverride> WeatherEffectOverrides => OverridesManager.WeatherEffectOverrides;
-
     // i would love to have weathers as an array with indexes corresponding to the enum values
     // but none is -1 so i have to do this
     public static List<Weather> Weathers => WeathersDictionary.Values.ToList();
@@ -101,49 +98,37 @@ namespace WeatherRegistry
       return string.Empty;
     }
 
+    #region WeightsManager
+
     internal static List<LevelWeatherType> GetPlanetPossibleWeathers(SelectableLevel level)
     {
-      List<LevelWeatherType> possibleWeathers = level
-        .randomWeathers.Where(randomWeather => randomWeather.weatherType != LevelWeatherType.None)
-        .Select(x => x.weatherType)
-        .Distinct()
-        .ToList();
-
-      // add None as a possible weather in front of the list
-      possibleWeathers.Insert(0, LevelWeatherType.None);
-
-      if (possibleWeathers == null || possibleWeathers.Count() == 0)
-      {
-        Plugin.logger.LogWarning("Level's random weathers are null");
-        return [];
-      }
-
-      return possibleWeathers;
+      return WeightsManager.GetPlanetPossibleWeathers(level);
     }
 
     public static WeightHandler<Weather, WeatherWeightType> GetPlanetWeightedList(SelectableLevel level)
     {
-      WeightHandler<Weather, WeatherWeightType> weightedList = new();
-      Logger logger = WeatherCalculation.Logger;
-
-      List<LevelWeatherType> weatherTypes = GetPlanetPossibleWeathers(level);
-
-      if (weatherTypes == null || weatherTypes.Count() == 0)
-      {
-        Plugin.logger.LogWarning("Level's random weathers are null");
-        return weightedList;
-      }
-
-      foreach (var weather in weatherTypes)
-      {
-        Weather typeOfWeather = GetWeather(weather);
-
-        (int weatherWeight, WeatherWeightType type) = typeOfWeather.GetWeightWithOrigin(level);
-        weightedList.Add(typeOfWeather, weatherWeight);
-      }
-
-      return weightedList;
+      return WeightsManager.GetPlanetWeightedList(level);
     }
+
+    public static (int weight, WeatherWeightType type) GetWeatherWeightForLevel(SelectableLevel level, Weather weather)
+    {
+      return WeightsManager.GetWeatherWeightForLevel(level, weather);
+    }
+
+    #endregion
+
+    #region OverridesManager
+
+    [Obsolete("Use OverridesManager.GetCurrentWeatherOverride instead")]
+    public static WeatherEffectOverride GetCurrentWeatherOverride(SelectableLevel level, Weather weather)
+    {
+      return OverridesManager.GetCurrentWeatherOverride(level, weather);
+    }
+
+    [Obsolete("Use OverridesManager.WeatherEffectOverrides instead")]
+    public static List<WeatherEffectOverride> WeatherEffectOverrides => OverridesManager.WeatherEffectOverrides;
+
+    #endregion
 
     public static Weather GetCurrentWeather(SelectableLevel level)
     {
@@ -172,12 +157,6 @@ namespace WeatherRegistry
       return GetCurrentWeather(level).Name;
     }
 
-    [Obsolete("Use OverridesManager.GetCurrentWeatherOverride instead")]
-    public static WeatherEffectOverride GetCurrentWeatherOverride(SelectableLevel level, Weather weather)
-    {
-      return OverridesManager.GetCurrentWeatherOverride(level, weather);
-    }
-
     public static string GetWeatherList()
     {
       List<WeatherListData> weathers = [];
@@ -192,54 +171,6 @@ namespace WeatherRegistry
         Formatting.None,
         new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }
       );
-    }
-
-    public static (int weight, WeatherWeightType type) GetWeatherWeightForLevel(SelectableLevel level, Weather weather)
-    {
-      Logger logger = WeatherCalculation.Logger;
-      var weatherWeight = weather.DefaultWeight;
-      WeatherWeightType weatherWeightType = WeatherWeightType.Default;
-
-      var previousWeather = WeatherManager.GetWeather(level.currentWeather);
-
-      if (previousWeather == null)
-      {
-        logger.LogError($"Previous weather is null for {level.name}");
-      }
-
-      // we have 3 weights possible:
-      // 1. level weight
-      // 2. weather-weather weights
-      // 3. default weight
-      // we want to execute them in this exact order
-
-      if (weather.LevelWeights.TryGetValue(level, out int levelWeight))
-      {
-        // (1) => level weight
-        // logger.LogDebug($"{this.Name} has level weight {levelWeight}");
-        weatherWeightType = WeatherWeightType.Level;
-        weatherWeight = levelWeight;
-      }
-      // try to get previous day weather (so - at this point - the current one)
-      // but not on first day because that's unreliable and random (from my testing)
-      else if (
-        previousWeather.WeatherWeights.TryGetValue(weather.VanillaWeatherType, out int weatherWeightFromWeather)
-        && StartOfRound.Instance.gameStats.daysSpent != 0
-      )
-      {
-        // (2) => weather-weather weights
-
-        // logger.LogDebug($"{this.Name} has weather>weather weight {weatherWeightFromWeather}");
-        weatherWeightType = WeatherWeightType.WeatherToWeather;
-        weatherWeight = weatherWeightFromWeather;
-      }
-      else
-      {
-        weatherWeightType = WeatherWeightType.Default;
-        // logger.LogDebug($"{this.Name} has default weight {weatherWeight}");
-      }
-
-      return (weatherWeight, weatherWeightType);
     }
   }
 }
