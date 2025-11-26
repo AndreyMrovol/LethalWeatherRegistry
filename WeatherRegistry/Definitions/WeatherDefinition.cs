666afbf3c18612c880e4f0bd92110235630e8a81
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2025-11-26 19:59:52 +0100

    common weather creation definition

---
+++ WeatherRegistry/Definitions/WeatherDefinition.cs
@@ -0,0 +1,33 @@
+using Newtonsoft.Json;
+using TMPro;
+using UnityEngine;
+using WeatherRegistry.Definitions;
+using WeatherRegistry.Editor;
+using WeatherRegistry.Enums;
+using WeatherRegistry.Helpers;
+using WeatherRegistry.Modules;
+
+namespace WeatherRegistry.Definitions
+{
+  [JsonObject(MemberSerialization.OptIn)]
+  public class WeatherDefinition : ScriptableObject
+  {
+    public string Name { get; set; }
+
+    public TMP_ColorGradient Color { get; set; } = ColorHelper.ToTMPColorGradient(UnityEngine.Color.cyan);
+
+    public ImprovedWeatherEffect Effect { get; set; }
+
+    public bool Enabled { get; set; }
+
+    public RegistryWeatherConfig Config { get; set; }
+
+    public WeatherOrigin Origin { get; private set; } = WeatherOrigin.WeatherRegistry;
+    public WeatherType Type { get; private set; } = WeatherType.Modded;
+
+    public override string ToString()
+    {
+      return $"<color=#{ColorHelper.ToHex(Color.topLeft)}>{Name} ({Origin}, {Type})</color>";
+    }
+  }
+}
