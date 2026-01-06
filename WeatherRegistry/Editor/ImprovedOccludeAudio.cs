88458fcc52a2226c2c9f6e2ffc79b1d27ff6c6bc
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-01-06 20:30:14 +0100

    Delete ImprovedOccludeAudio.cs

--- WeatherRegistry/Editor/ImprovedOccludeAudio.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace WeatherRegistry.Editor
-{
-  [Obsolete("ImprovedOccludeAudio has been moved to WeatherRegistry.Components namespace. This class is kept for backward compatibility.")]
-  public class ImprovedOccludeAudio : Components.ImprovedOccludeAudio { }
-}
