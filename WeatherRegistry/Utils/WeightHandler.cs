63a9383625b56665566b66061fd303c0e53235f1
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-02-04 23:12:21 +0100

    display weight errors under registry

--- WeatherRegistry/Utils/WeightHandler.cs
+++ WeatherRegistry/Utils/WeightHandler.cs
@@ -5,6 +5,7 @@ namespace WeatherRegistry.Utils
   public class WeightHandler<T, OT> : MrovLib.WeightHandler<T>
   {
     private Dictionary<T, OT> originDict = [];
+    public override MrovLib.Logger logger => Plugin.debugLogger;
 
     public OT GetOrigin(T item)
     {
