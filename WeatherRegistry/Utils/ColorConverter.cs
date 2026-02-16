8f04c2c4152af5ef7ec6633e996ca39577afb89f
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-02-16 12:16:21 +0100

    added CreateColorGradientInstance

--- WeatherRegistry/Utils/ColorConverter.cs
+++ WeatherRegistry/Utils/ColorConverter.cs
@@ -20,6 +20,11 @@ namespace WeatherRegistry.Utils
       return gradient;
     }
 
+    public static TMP_ColorGradient CreateColorGradientInstance()
+    {
+      return ScriptableObject.CreateInstance<TMP_ColorGradient>();
+    }
+
     public static string ToHex(Color color)
     {
       return ColorUtility.ToHtmlStringRGB(color);
